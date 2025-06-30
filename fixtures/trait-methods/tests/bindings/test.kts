77bde5fe4d6c4de0dda9a935ead82f38d4bb2bef
Mark Hammond <mhammond@skippinet.com.au>
2025-06-29 21:55:22 -0400

    kotlin

--- fixtures/trait-methods/tests/bindings/test.kts
+++ fixtures/trait-methods/tests/bindings/test.kts
@@ -14,3 +14,16 @@ assert(ProcTraitMethods("a") < ProcTraitMethods("b"))
 assert(m < TraitMethods("z"))
 assert(m <= TraitMethods("z"))
 assert(TraitMethods("z") > m)
+
+// Enums
+assert((UdlEnum::S)("hello").toString() == "UdlEnum::S { s: \"hello\" }")
+assert((UdlEnum::I)(1).toString() == "UdlEnum::I { i: 1 }")
+assert((TraitEnum::S)("hello").toString() == "TraitEnum::S(\"hello\")")
+assert((TraitEnum::I)(1).toString() == "TraitEnum::I(1)")
+
+assert((UdlEnum::S)("hello") == (UdlEnum::S)("hello"))
+assert((UdlEnum::S)("hello") == (UdlEnum::S)("other"))
+assert((UdlEnum::S)("hello") < (UdlEnum::I)(0))
+assert((TraitEnum::I)(1) == (TraitEnum::I)(1))
+assert((TraitEnum::I)(1) == (TraitEnum::I)(2))
+assert((TraitEnum::S)("hello") < (TraitEnum::I)(0))
